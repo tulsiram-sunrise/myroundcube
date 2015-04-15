47c1b73dc03c91129bf9419598fd2cab7779c957
roland.liebl <roland.liebl@gmail.com>
2015-04-15 09:08:35 +0000

    Remove plugins

--- plugins/carddav/SQL/mysql.20140410.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `carddav_contactgroupmembers` ADD INDEX(`contact_id`);
-UPDATE `system` SET `value`='initial|20130903|20131110|20140406|20140410' WHERE `name`='myrc_carddav';
--- plugins/carddav/SQL/sqlite.20140809.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE carddav_server ADD subscribed tinyint(1) NOT NULL DEFAULT 1;
-UPDATE system SET value='initial|20130903|20131110|20140406|20140410|20140411|20140809' WHERE name='myrc_carddav';
