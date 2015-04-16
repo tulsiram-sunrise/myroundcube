071562034d18addae9a6f0b74f7a7049d8673f22
roland.liebl@gmail.com <roland.liebl@gmail.com@65159183-c03d-18bb-21a0-0111efd3f6ac>
2015-04-16 05:02:08 +0000

    Remove plugins

--- trunk/plugins/savepassword/SQL/pgsql.initial.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "system" (
-    name varchar(64) NOT NULL PRIMARY KEY,
-    value text
-);
-INSERT INTO "system" (name, value) VALUES ('myrc_savepassword', 'initial');
-ALTER TABLE users ADD COLUMN password TEXT NULL;
